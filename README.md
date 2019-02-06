65a93a4bc2f59a263e3707f56d17532977d87494
Arthur Dias <arthurdias.trad@outlook.com>
2019-02-06 00:53:06 -0200

    First commit of the basic version of the app

---
+++ README.md
@@ -0,0 +1,5 @@
+# Interpreter timer
+<br><br>
+A simple app created to divide the timeslots for turnover in the interpreters' both.<br>
+This basic version simply prints out the periods of time each interpreter will work, according to a block of time to be determined by the interpreters (usually from 15 to 30 minutes).<br>
+Future versions will include a GUI, the possibility to choose between 12-hour or 24-hour format, and the possitibility to account for lunch/coffee breaks.
---
+++ interpreter_timer.py
@@ -0,0 +1,34 @@
+from datetime import datetime as dt, timedelta as td
+
+# Get interpreter names
+first_terp = input("Interpreter 1: ")
+second_terp =  input("Interpreter 2: ")
+
+# Get start and end times
+start_time_string = input("Enter starting time (HH:MM, 24-h format): ")
+end_time_string = input("Enter starting time (HH:MM, 24-h format): ")
+
+# Create start and end times datetime objects
+start_time = dt.strptime(start_time_string, "%H:%M")
+end_time = dt.strptime(end_time_string, "%H:%M")
+
+# Get block size
+block_string = input("Enter the time until turnover (in minutes): ")
+
+# Create timedelta object
+block = td(minutes=int(block_string))
+
+# Create loops to print out interpreter's turns
+count = 0
+while start_time < end_time:
+    if count % 2 == 0:
+        terp = first_terp
+    else:
+        terp = second_terp
+    
+    temp = start_time + block
+    print("{}: {} - {}".format(terp, start_time.strftime("%H:%M"), temp.strftime("%H:%M")))
+    start_time = temp
+    count += 1
+
+
